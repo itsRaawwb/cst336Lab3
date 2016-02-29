4fa679387ff9457faac84b83370ce62bfc304c40
David D <ddemers@csumb.edu>
2016-02-29 07:01:50 +0000

    almost done?

--- game.php
+++ game.php
@@ -2,9 +2,11 @@
 <html>
     <head>
         <title>Play Some Silverjack!</title>
+        <link rel="shortcut icon" href="https://csumb.edu/sites/default/files/pixelotter.png" type="image/png">
         <link rel="stylesheet" type="css" href="css/main.css">
     </head>
-    <body>
+    <body class = "b2">
+        <h1>Silverjack</h1>
         <?php
             main();
         ?>
@@ -16,6 +18,13 @@
             <input type="hidden" name="p4" value="<?=  $_POST["p4"] ?>" />
             <input type="submit" value="Play again?" />
         </form>
+        <footer>
+        <HR WIDTH="80%" NOSHADE>
+        This site is for the purpose of CST 336. The information may not be accurate.
+        <br>2016
+        <br>
+        <img src="assets/csumb.png" height="50" />
+    </footer>
     </body>
 </html>
 
@@ -200,7 +209,7 @@
         $player4["winner"] = $winningTables[3];
             //display table
             
-            
+        echo '<center>';
         echo '<div>';
         echo '<table border ="1" class="gameTable">';
             //header row
@@ -232,11 +241,6 @@
                 echo '</td>';
                 
                 echo '<td>';
-                $temp = getCardValue($player1["card2"]);
-                echo $temp[2];
-                echo '</td>';
-                
-                echo '<td>';
                 $temp = getCardValue($player1["card3"]);
                 echo $temp[2];
                 echo '</td>';
@@ -257,8 +261,12 @@
                 echo '</td>';
                 
                 echo '<td>';
+                echo $player1["points"];
+                echo '</td>';
+                
+                echo '<td>';
                 if($player1["winner"] == 1){
-                    echo "Winner";
+                    echo "Winner: ". $player1["name"];
                 }
                 else{
                     echo "Loser";
@@ -273,11 +281,6 @@
                 
                 echo '<td>';
                 $temp = getCardValue($player2["card1"]);
-                echo $temp[2];
-                echo '</td>';
-                
-                echo '<td>';
-                $temp = getCardValue($player2["card2"]);
                 echo $temp[2];
                 echo '</td>';
                 
@@ -307,8 +310,12 @@
                 echo '</td>';
                 
                 echo '<td>';
+                echo $player2["points"];
+                echo '</td>';
+                
+                echo '<td>';
                 if($player2["winner"] == 1){
-                    echo "Winner";
+                    echo "Winner: ". $player2["name"];
                 }
                 else{
                     echo "Loser";
@@ -323,11 +330,6 @@
                 
                 echo '<td>';
                 $temp = getCardValue($player3["card1"]);
-                echo $temp[2];
-                echo '</td>';
-                
-                echo '<td>';
-                $temp = getCardValue($player3["card2"]);
                 echo $temp[2];
                 echo '</td>';
                 
@@ -357,8 +359,12 @@
                 echo '</td>';
                 
                 echo '<td>';
+                echo $player3["points"];
+                echo '</td>';
+                
+                echo '<td>';
                 if($player3["winner"] == 1){
-                    echo "Winner";
+                    echo "Winner: ". $player3["name"];
                 }
                 else{
                     echo "Loser";
@@ -373,11 +379,6 @@
                 
                 echo '<td>';
                 $temp = getCardValue($player4["card1"]);
-                echo $temp[2];
-                echo '</td>';
-                
-                echo '<td>';
-                $temp = getCardValue($player4["card2"]);
                 echo $temp[2];
                 echo '</td>';
                 
@@ -407,8 +408,12 @@
                 echo '</td>';
                 
                 echo '<td>';
+                echo $player4["points"];
+                echo '</td>';
+                
+                echo '<td>';
                 if($player4["winner"] == 1){
-                    echo "Winner";
+                    echo "Winner: ". $player4["name"];
                 }
                 else{
                     echo "Loser";
@@ -417,6 +422,7 @@
             echo '</tr>'; // end fourth player
         echo '</table>';
         echo '</div>';
+        echo '</center>';
 
     }
     
@@ -430,219 +436,219 @@
                 return($cardValue);
                 break;
             case 2:
-                $cardValue=[1,"AoH"];
+                $cardValue=[1,"AoH","<img width = 75 src = 'assets/cards/hearts/1.png' />"];
                 return($cardValue);
                 break;
             case 2:
-                $cardValue=[1,"AoD"];
+                $cardValue=[1,"AoD","<img width = 75 src = 'assets/cards/diamonds/1.png' />"];
                 return($cardValue);
                 break;
             case '4':
-                $cardValue=[1,"AoC"];
+                $cardValue=[1,"AoC","<img width = 75 src = 'assets/cards/clubs/1.png' />"];
                 return($cardValue);
                 break;
             //2's
             case '5':
-                $cardValue=[2,"2oS"];
+                $cardValue=[2,"2oS","<img width = 75 src = 'assets/cards/spades/2.png' />"];
                 return($cardValue);
                 break;
             case '6':
-                $cardValue=[2,"2oH"];
+                $cardValue=[2,"2oH","<img width = 75 src = 'assets/cards/hearts/2.png' />"];
                 return($cardValue);
                 break;
             case '7':
-                $cardValue=[2,"2oD"];
+                $cardValue=[2,"2oD","<img width = 75 src = 'assets/cards/diamonds/2.png' />"];
                 return($cardValue);
                 break;
             case '8':
-                $cardValue=[2,"2oC"];
+                $cardValue=[2,"2oC","<img width = 75 src = 'assets/cards/clubs/2.png' />"];
                 return($cardValue);
                 break;
             //3's
             case '9':
-                $cardValue=[3,"3oS"];
+                $cardValue=[3,"3oS","<img width = 75 src = 'assets/cards/spades/3.png' />"];
                 return($cardValue);
                 break;
             case '10':
-                $cardValue=[3,"3oH"];
+                $cardValue=[3,"3oH","<img width = 75 src = 'assets/cards/hearts/3.png' />"];
                 return($cardValue);
                 break;
             case '11':
-                $cardValue=[3,"3oD"];
+                $cardValue=[3,"3oD","<img width = 75 src = 'assets/cards/diamonds/3.png' />"];
                 return($cardValue);
                 break;
             case '12':
-                $cardValue=[3,"3oC"];
+                $cardValue=[3,"3oC","<img width = 75 src = 'assets/cards/clubs/3.png' />"];
                 return($cardValue);
                 break;
             //4's
             case '13':
-                $cardValue=[4,"4oS"];
+                $cardValue=[4,"4oS","<img width = 75 src = 'assets/cards/spades/4.png' />"];
                 return($cardValue);
                 break;
             case '14':
-                $cardValue=[4,"4oD"];
+                $cardValue=[4,"4oD","<img width = 75 src = 'assets/cards/diamonds/4.png' />"];
                 return($cardValue);
                 break;
             case '15':
-                $cardValue=[4,"4oH"];
+                $cardValue=[4,"4oH","<img width = 75 src = 'assets/cards/hearts/4.png' />"];
                 return($cardValue);
                 break;
             case '16':
-                $cardValue=[4,"4oC"];
+                $cardValue=[4,"4oC","<img width = 75 src = 'assets/cards/clubs/4.png' />"];
                 return($cardValue);
                 break;
             //5's
             case '17':
-                $cardValue=[5,"5oS"];
+                $cardValue=[5,"5oS","<img width = 75 src = 'assets/cards/spades/5.png' />"];
                 return($cardValue);
                 break;
             case '18':
-                $cardValue=[5,"5oD"];
+                $cardValue=[5,"5oD","<img width = 75 src = 'assets/cards/diamonds/5.png' />"];
                 return($cardValue);
                 break;
             case '19':
-                $cardValue=[5,"5oH"];
+                $cardValue=[5,"5oH","<img width = 75 src = 'assets/cards/hearts/5.png' />"];
                 return($cardValue);
                 break;
             case '20':
-                $cardValue=[5,"5oC"];
+                $cardValue=[5,"5oC","<img width = 75 src = 'assets/cards/clubs/5.png' />"];
                 return($cardValue);
                 break;
             //6's
             case '21':
-                $cardValue=[6,"6oS"];
+                $cardValue=[6,"6oS","<img width = 75 src = 'assets/cards/spades/6.png' />"];
                 return($cardValue);
                 break;
             case '22':
-                $cardValue=[6,"6oD"];
+                $cardValue=[6,"6oD","<img width = 75 src = 'assets/cards/diamonds/6.png' />"];
                 return($cardValue);
                 break;
             case '23':
-                $cardValue=[6,"6oH"];
+                $cardValue=[6,"6oH","<img width = 75 src = 'assets/cards/hearts/6.png' />"];
                 return($cardValue);
                 break;
             case '24':
-                $cardValue=[6,"6oC"];
+                $cardValue=[6,"6oC","<img width = 75 src = 'assets/cards/clubs/6.png' />"];
                 return($cardValue);
                 break;
             //7's
             case '25':
-                $cardValue=[7,"7oS"];
+                $cardValue=[7,"7oS","<img width = 75 src = 'assets/cards/spades/7.png' />"];
                 return($cardValue);
                 break;
             case '26':
-                $cardValue=[7,"7oD"];
+                $cardValue=[7,"7oD","<img width = 75 src = 'assets/cards/diamonds/7.png' />"];
                 return($cardValue);
                 break;
             case '27':
-                $cardValue=[7,"7oH"];
+                $cardValue=[7,"7oH","<img width = 75 src = 'assets/cards/hearts/7.png' />"];
                 return($cardValue);
                 break;
             case '28':
-                $cardValue=[7,"7oC"];
+                $cardValue=[7,"7oC","<img width = 75 src = 'assets/cards/clubs/7.png' />"];
                 return($cardValue);
                 break;
             //8's
             case '29':
-                $cardValue=[8,"8oS"];
+                $cardValue=[8,"8oS","<img width = 75 src = 'assets/cards/spades/8.png' />"];
                 return($cardValue);
                 break;
             case '30':
-                $cardValue=[8,"8oD"];
+                $cardValue=[8,"8oD","<img width = 75 src = 'assets/cards/diamonds/8.png' />"];
                 return($cardValue);
                 break;
             case '31':
-                $cardValue=[8,"8oH"];
+                $cardValue=[8,"8oH","<img width = 75 src = 'assets/cards/hearts/8.png' />"];
                 return($cardValue);
                 break;
             case '32':
-                $cardValue=[8,"8oC"];
+                $cardValue=[8,"8oC","<img width = 75 src = 'assets/cards/clubs/8.png' />"];
                 return($cardValue);
                 break;
             //9's
             case '33':
-                $cardValue=[9,"9oS"];
+                $cardValue=[9,"9oS","<img width = 75 src = 'assets/cards/spades/9.png' />"];
                 return($cardValue);
                 break;
             case '34':
-                $cardValue=[9,"9oD"];
+                $cardValue=[9,"9oD","<img width = 75 src = 'assets/cards/diamonds/9.png' />"];
                 return($cardValue);
                 break;
             case '35':
-                $cardValue=[9,"9oH"];
+                $cardValue=[9,"9oH","<img width = 75 src = 'assets/cards/hearts/9.png' />"];
                 return($cardValue);
                 break;
             case '36':
-                $cardValue=[9,"9oC"];
+                $cardValue=[9,"9oC","<img width = 75 src = 'assets/cards/clubs/9.png' />"];
                 return($cardValue);
                 break;
             //10's
             case '37':
-                $cardValue=[10,"10oS"];
+                $cardValue=[10,"10oS","<img width = 75 src = 'assets/cards/spades/10.png' />"];
                 return($cardValue);
                 break;
             case '38':
-                $cardValue=[10,"10oD"];
+                $cardValue=[10,"10oD","<img width = 75 src = 'assets/cards/diamonds/10.png' />"];
                 return($cardValue);
                 break;
             case '39':
-                $cardValue=[10,"10oH"];
+                $cardValue=[10,"10oH","<img width = 75 src = 'assets/cards/hearts/10.png' />"];
                 return($cardValue);
                 break;
             case '40':
-                $cardValue=[10,"10oC"];
+                $cardValue=[10,"10oC","<img width = 75 src = 'assets/cards/clubs/10.png' />"];
                 return($cardValue);
                 break;
             //Jacks
             case '41':
-                $cardValue=[11,"JoS"];
+                $cardValue=[11,"JoS","<img width = 75 src = 'assets/cards/spades/11.png' />"];
                 return($cardValue);
                 break;
             case '42':
-                $cardValue=[11,"JoD"];
+                $cardValue=[11,"JoD","<img width = 75 src = 'assets/cards/diamonds/11.png' />"];
                 return($cardValue);
                 break;
             case '43':
-                $cardValue=[11,"JoH"];
+                $cardValue=[11,"JoH","<img width = 75 src = 'assets/cards/hearts/11.png' />"];
                 return($cardValue);
                 break;
             case '44':
-                $cardValue=[11,"JoC"];
+                $cardValue=[11,"JoC","<img width = 75 src = 'assets/cards/clubs/11.png' />"];
                 return($cardValue);
                 break;
             //Queens
             case '45':
-                $cardValue=[12,"QoS"];
+                $cardValue=[12,"QoS","<img width = 75 src = 'assets/cards/spades/12.png' />"];
                 return($cardValue);
                 break;
             case '46':
-                $cardValue=[12,"QoD"];
+                $cardValue=[12,"QoD","<img width = 75 src = 'assets/cards/diamonds/12.png' />"];
                 return($cardValue);
                 break;
             case '47':
-                $cardValue=[12,"QoH"];
+                $cardValue=[12,"QoH","<img width = 75 src = 'assets/cards/hearts/12.png' />"];
                 return($cardValue);
                 break;
             case '48':
-                $cardValue=[12,"QoC"];
+                $cardValue=[12,"QoC","<img width = 75 src = 'assets/cards/clubs/12.png' />"];
                 return($cardValue);
                 break;
             //Kings
             case '49':
-                $cardValue=[13,"KoS"];
+                $cardValue=[13,"KoS","<img width = 75 src = 'assets/cards/spades/13.png' />"];
                 return($cardValue);
                 break;
             case '50':
-                $cardValue=[13,"KoD"];
+                $cardValue=[13,"KoD","<img width = 75 src = 'assets/cards/diamonds/13.png' />"];
                 return($cardValue);
                 break;
             case '51':
-                $cardValue=[13,"KoH"];
+                $cardValue=[13,"KoH","<img width = 75 src = 'assets/cards/hearts/13.png' />"];
                 return($cardValue);
                 break;
             case '52':
-                $cardValue=[13,"KoC"];
+                $cardValue=[13,"KoC","<img width = 75 src = 'assets/cards/clubs/13.png' />"];
                 return($cardValue);
                 break;
         }
